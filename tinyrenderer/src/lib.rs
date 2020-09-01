eedcdecbbe0a08f98a1a4c69510b96f08bae5a1f
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2020-09-01 20:58:06 +0300

    tinyrenderer: Clean up lib.rs

---
+++ tinyrenderer/src/lib.rs
@@ -0,0 +1,48 @@
+extern crate tgaimage;
+pub mod geometry;
+pub mod model;
+
+use tgaimage::{TGAColor, TGAImage};
+
+pub fn line(
+    mut x0: i32,
+    mut y0: i32,
+    mut x1: i32,
+    mut y1: i32,
+    color: TGAColor,
+    image: &mut TGAImage,
+) {
+    let mut steep = false;
+
+    if (x0 - x1).abs() < (y0 - y1).abs() {
+        std::mem::swap(&mut x0, &mut y0);
+        std::mem::swap(&mut x1, &mut y1);
+        steep = true;
+    }
+
+    if x0 > x1 {
+        std::mem::swap(&mut x0, &mut x1);
+        std::mem::swap(&mut y0, &mut y1);
+    }
+
+    let dx = x1 - x0;
+    let dy = y1 - y0;
+    let derror2 = dy.abs() * 2;
+    let mut error2 = 0;
+    let mut y = y0;
+
+    for x in x0..=x1 {
+        if steep {
+            image.set(y as u32, x as u32, color);
+        } else {
+            image.set(x as u32, y as u32, color);
+        }
+
+        error2 += derror2;
+
+        if error2 > dx {
+            y += if y1 > y0 { 1 } else { -1 };
+            error2 -= dx * 2;
+        }
+    }
+}
