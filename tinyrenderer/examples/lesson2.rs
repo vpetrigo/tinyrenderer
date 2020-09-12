52e2b7ee4e9bca12056c5f307b29893cc45aae62
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2020-09-12 11:51:48 +0300

    tinyrenderer: lesson 2 update with new triangles

--- tinyrenderer/examples/lesson2.rs
+++ tinyrenderer/examples/lesson2.rs
@@ -1,6 +1,6 @@
 use tgaimage::{TGAColor, TGAImage, TGAImageFormat};
 use tinyrenderer::geometry::Vector2Int;
-use tinyrenderer::{triangle, line};
+use tinyrenderer::{line, triangle};
 
 const WHITE: TGAColor = TGAColor::new_rgba(255, 255, 255, 255);
 const RED: TGAColor = TGAColor::new_rgba(255, 0, 0, 0);
@@ -17,13 +17,16 @@ fn main() {
     let v7 = Vector2Int::new(260, 500);
     let v8 = Vector2Int::new(120, 420);
     let v9 = Vector2Int::new(360, 750);
+    let v10 = Vector2Int::new(410, 410);
+    let v11 = Vector2Int::new(320, 520);
+    let v12 = Vector2Int::new(780, 410);
     let mut image = TGAImage::new(800, 800, TGAImageFormat::RGB);
 
     triangle(v1, v2, v3, &WHITE, &mut image);
     triangle(v4, v5, v6, &RED, &mut image);
     triangle(v7, v8, v9, &GREEN, &mut image);
-    line(120, 0, 120, 800, &WHITE, &mut image);
+    triangle(v10, v11, v12, &WHITE, &mut image);
     image
-        .write_tga_file("triangles.tga", false, true)
+        .write_tga_file("triangles.tga", true, true)
         .expect("Cannot write image");
 }
