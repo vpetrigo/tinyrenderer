0202795135814156f7b33cfe990cf7cb7e3eab90
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2020-09-13 17:21:05 +0300

    tinyrenderer: lib: Add triangle rasterization with barycentric coordinates

--- tinyrenderer/src/lib.rs
+++ tinyrenderer/src/lib.rs
@@ -1,6 +1,6 @@
 use tgaimage::{TGAColor, TGAImage};
 
-use crate::geometry::{Vector2, Vector2Int};
+use crate::geometry::{Vector2, Vector2Int, Vector3F32};
 
 pub mod geometry;
 pub mod model;
@@ -44,6 +44,71 @@ pub fn line(
         if error2 > dx {
             y += if y1 > y0 { 1 } else { -1 };
             error2 -= dx * 2;
+        }
+    }
+}
+
+fn barycentric(points: &[Vector2Int; 3], point: Vector2Int) -> Vector3F32 {
+    let u = Vector3F32::new(
+        (points[2].get_x() - points[0].get_x()) as f32,
+        (points[1].get_x() - points[0].get_x()) as f32,
+        (points[0].get_x() - point.get_x()) as f32,
+    ) ^ Vector3F32::new(
+        (points[2].get_y() - points[0].get_y()) as f32,
+        (points[1].get_y() - points[0].get_y()) as f32,
+        (points[0].get_y() - point.get_y()) as f32,
+    );
+
+    if u.get_z().abs() < 1.0 {
+        return Vector3F32::new(-1.0, 1.0, 1.0);
+    }
+
+    Vector3F32::new(
+        1.0 - (u.get_x() + u.get_y()) / u.get_z(),
+        u.get_y() / u.get_z(),
+        u.get_x() / u.get_z(),
+    )
+}
+
+/// Fill triangle with calculating barycentric coordinates
+/// for properly determine which pixels should be filled
+/// Arguments:
+/// * `v1` - Vertice of a triangle
+/// * `v2` - Vertice of a triangle
+/// * `v3` - Vertice of a triangle
+/// * `color` - color to fill triangle with
+/// * `image` - image to draw triangle in
+pub fn triangle_barycentric(
+    v1: Vector2Int,
+    v2: Vector2Int,
+    v3: Vector2Int,
+    color: &TGAColor,
+    image: &mut TGAImage,
+) {
+    let mut boundary_box_min = Vector2Int::new(
+        (image.get_width() - 1) as i32,
+        (image.get_height() - 1) as i32,
+    );
+    let mut boundary_box_max = Vector2Int::new(0, 0);
+    let clamp = boundary_box_min;
+    let points = [v1, v2, v3];
+
+    for p in &points {
+        *boundary_box_min.get_x_as_mut() = 0.max(boundary_box_min.get_x().min(p.get_x()));
+        *boundary_box_min.get_y_as_mut() = 0.max(boundary_box_min.get_y().min(p.get_y()));
+        *boundary_box_max.get_x_as_mut() =
+            clamp.get_x().min(boundary_box_max.get_x().max(p.get_x()));
+        *boundary_box_max.get_y_as_mut() =
+            clamp.get_y().min(boundary_box_max.get_y().max(p.get_y()));
+    }
+
+    for x in boundary_box_min.get_x()..=boundary_box_max.get_x() {
+        for y in boundary_box_min.get_y()..=boundary_box_max.get_y() {
+            let bc_screen = barycentric(&points, Vector2::new(x, y));
+
+            if bc_screen.get_x() >= 0.0 && bc_screen.get_y() >= 0.0 && bc_screen.get_z() >= 0.0 {
+                image.set(x as u32, y as u32, color);
+            }
         }
     }
 }
