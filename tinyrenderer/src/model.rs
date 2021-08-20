bf4ab6c71d3b66253eb1b6c06bbb8dd40497d4b7
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2021-08-20 23:50:49 +0300

    tinyrederer: model: Clean up model implementation

--- tinyrenderer/src/model.rs
+++ tinyrenderer/src/model.rs
@@ -1,13 +1,32 @@
 use crate::geometry::Vector3F32;
-use std::fs::File;
-use std::io;
-use std::io::{BufRead, BufReader};
-use std::str::FromStr;
+use std::{
+    fs::File,
+    io,
+    io::{BufRead, BufReader},
+    str::{FromStr, SplitWhitespace},
+};
 use tgaimage::TGAImage;
+
+#[derive(Default)]
+struct ModelFace {
+    verts_index: [u32; 3],
+    uv: [u32; 3],
+    norm: [u32; 3],
+}
+
+impl ModelFace {
+    fn new(verts_index: [u32; 3]) -> Self {
+        ModelFace {
+            verts_index,
+            uv: [0; 3],
+            norm: [0; 3],
+        }
+    }
+}
 
 pub struct Model {
     verts: Vec<Vector3F32>,
-    faces: Vec<Vec<u32>>,
+    faces: Vec<ModelFace>,
 }
 
 impl Model {
@@ -18,33 +37,51 @@ impl Model {
         let mut faces = vec![];
 
         for line in reader.lines() {
-            let line = line.unwrap();
-            if line.starts_with("v ") {
-                let mut data = line.splitn(4, " ").into_iter().skip(1);
-                let mut coords = [f32::default(); 3];
+            let (_line, mut words) = match line {
+                Ok(ref line) => (&line[..], line[..].split_whitespace()),
+                Err(_e) => return Err(_e),
+            };
+            // let line = line.unwrap();
 
-                coords
-                    .iter_mut()
-                    .for_each(|elem| *elem = f32::from_str(data.next().unwrap()).unwrap());
-
-                verts.push(Vector3F32::new(coords[0], coords[1], coords[2]));
-            } else if line.starts_with("f ") {
-                let data = line.split(" ").into_iter().skip(1);
-                let mut face_coords: [u32; 3] = [0; 3];
-                let mut index = 0;
-
-                data.for_each(|s| {
-                    face_coords[index] =
-                        u32::from_str(s.split("/").into_iter().nth(0).unwrap()).unwrap() - 1;
-                    index += 1;
-                });
-
-                assert_eq!(index, 3);
-                faces.push(face_coords.into());
+            match words.next() {
+                Some("v") => Model::process_vertice(&mut words, &mut verts),
+                Some("f") => Model::process_face(&mut words, &mut faces),
+                Some("#") | None | Some(_) => continue,
             }
         }
 
         Ok(Model { verts, faces })
+    }
+
+    fn process_vertice(words: &mut SplitWhitespace, vertices: &mut Vec<Vector3F32>) {
+        let mut coords = [f32::default(); 3];
+        coords
+            .iter_mut()
+            .zip(words.into_iter())
+            .enumerate()
+            .for_each(|(i, (c, w))| {
+                *c = f32::from_str(w).unwrap();
+                assert!(i < 3);
+            });
+        vertices.push(Vector3F32::new_from_array(&coords));
+    }
+
+    fn process_face(words: &mut SplitWhitespace, faces: &mut Vec<ModelFace>) {
+        let mut model_face = ModelFace::default();
+
+        words.into_iter().enumerate().for_each(|(i, word)| {
+            let mut it = word
+                .split("/")
+                .map(|n| u32::from_str(n).unwrap() - 1)
+                .into_iter();
+
+            model_face.verts_index[i] = it.next().unwrap();
+            model_face.uv[i] = it.next().unwrap();
+            model_face.norm[i] = it.next().unwrap();
+            assert!(i < 3);
+        });
+
+        faces.push(model_face);
     }
 
     pub fn load_texture(&mut self, _filename: &str, _image: &mut TGAImage) {
@@ -67,11 +104,11 @@ impl Model {
         &mut self.verts[index]
     }
 
-    pub fn face(&self, index: usize) -> &Vec<u32> {
-        &self.faces[index]
+    pub fn face(&self, index: usize) -> &[u32; 3] {
+        &self.faces[index].verts_index
     }
 
-    pub fn face_mut(&mut self, index: usize) -> &mut Vec<u32> {
-        &mut self.faces[index]
+    pub fn face_mut(&mut self, index: usize) -> &mut [u32; 3] {
+        &mut self.faces[index].verts_index
     }
 }
